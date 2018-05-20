d2967bc817fa148115d685458089642ee25eb41e
sipian <cs15btech11019@iith.ac.in>
2018-05-20 01:26:13 -0400

    prow test-check

---
+++ prow-test/hello-world.go
@@ -0,0 +1,10 @@
+// Our first program will print the classic "hello world"
+// message. Here's the full source code.
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("hello world")
+}
+
