a134f639ddea461b965690d7cbcdec6ffa991d91
sipian <cs15btech11019@iith.ac.in>
2018-05-24 18:56:35 -0400

    update repo name

--- prow/common.sh
+++ prow/common.sh
@@ -29,10 +29,9 @@ export PATH="$GOPATH/bin:/usr/local/go/bin:$PATH"
 mkdir -p "$GOPATH/src" "$GOPATH/bin" && chmod -R 777 "$GOPATH"
 echo "go version :: $(go version)"
 echo "************************ getting all dependencies to run tests. This step will not be needed when running from original prometheus repo *********************"
-tree -d ~/
 #go get ./...
 #running tests
-repoName="github.com/prometheus/prometheus"
+repoName="github.com/sipian/prometheus"
 if [[ -z "${TEST}" ]]; then
   tests=0
 elif [ "${TEST}" = "true" ]; then
