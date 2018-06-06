441dec4f23059c715d89ad7a88fd05302459e78c
Harsh Agarwal <cs15btech11019@iith.ac.in>
2018-06-06 16:55:45 -0400

    fix env variable name
    Signed-off-by: Harsh Agarwal <cs15btech11019@iith.ac.in>

--- prow/benchmark_build.sh
+++ prow/benchmark_build.sh
@@ -5,7 +5,7 @@ make build
 
 #pushing docker image
 docker login -u _json_key --password-stdin https://gcr.io < /etc/serviceaccount/service-account.json
-docker build --pull=true --rm=true -t $PROW_BENCHMARK_DOCKER_IMAGE
+docker build --pull=true --rm=true -t $PROW_BENCHMARK_DOCKER_IMAGE ./
 docker push $PROW_BENCHMARK_DOCKER_IMAGE
 
 #cleaning node docker
